be30973bb0e08ef7e3601b7c26b5b8eb0f9fc657
repo-ranger21 <chris.peterson6995@gmail.com>
2025-07-31 14:59:15 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,10 +1,15 @@
 from typing import List, Optional
 
-__version__ = "25.1.1"
+from pip._internal.utils import _log
+
+# init_logging() must be called before any call to logging.getLogger()
+# which happens at import of most modules.
+_log.init_logging()
 
 
 def main(args: Optional[List[str]] = None) -> int:
-    """This is an internal API only meant for use by pip's own console scripts.
+    """This is preserved for old console scripts that may still be referencing
+    it.
 
     For additional details, see https://github.com/pypa/pip/issues/7498.
     """
